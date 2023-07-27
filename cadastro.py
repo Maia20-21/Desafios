08075b45c9cbc7089ac6a3717262095bbf8f65c6
bruninhaa19 <br2020maia@gmail.com>
2023-07-27 14:52:36 -0300

    Update cadastro.py

---
+++ cadastro.py
@@ -0,0 +1,8 @@
+nome = int(input("Digite seu nome"))
+sobrenome = int(input("Digite seu sobrenome"))
+idade = int(input("Digite sua idade"))
+signo = int(input("Digite seu signo"))
+sonho = int(input("Digite seu sonho"))
+
+print("Cadastro realizado com sucesso")
+
