ab0a8326567ff9edc4d5bc30a5a46d6cff9e4e8b
bruninhaa19 <br2020maia@gmail.com>
2023-07-27 15:06:35 -0300

    Update banco.py

--- banco.py
+++ banco.py
@@ -1,3 +1,5 @@
 saldo = int(input("Digite seu saldo bancário"))
 valor = int(input("Digite o valor que você deseja retirar da sua conta"))
 
+if valor > saldo: print("Saldo insuficiente para essa transação")
+else: print("Saldo retirado. Verifique o valor sacado")
